252159c67c3dfa508fd72f46e204770756535993
Abdulsalam Alali <alaliab2@cse.msu.edu>
2019-08-29 20:56:22 +0300

    day4

---
+++ day4.py
@@ -0,0 +1,14 @@
+x = 22
+y = 14.3
+z = 2+1j
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+a = float(x)
+b = int(y)
+c = complex(y)
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
