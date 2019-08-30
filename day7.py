617c7cdd72a10440cd346f8751afd4b4b2e81855
Abdulsalam Alali <alaliab2@cse.msu.edu>
2019-08-30 23:40:48 +0300

    day7

---
+++ day7.py
@@ -0,0 +1,13 @@
+# Python String
+print('Abdulsalam')
+print("Al Ali")
+a = 'salam'
+print(a)
+b = "I am writing multi-line \
+    string in here. In the provided example \
+    they didn not use the back slash \
+    However, it is necessary here"
+
+c = 'How are you?'
+print(c[0])
+print(c[1:5])
