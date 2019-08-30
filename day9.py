7a98414c373f2faaae46b942a7c3471ca8fdc128
Abdulsalam Alali <alaliab2@cse.msu.edu>
2019-08-30 23:52:14 +0300

    day9

---
+++ day9.py
@@ -0,0 +1,7 @@
+# String Format
+print("my age is {}".format(234))
+print("the number {} is bigger than {}".format(5,3))
+# you could specify the location
+a = 3
+b = 3.14
+print("{1} is a an approximate value for {0}".format(b,a))
