dbad43431345c447122aa2ca514abdb3f9b6bc6a
Abdulsalam Alali <alaliab2@cse.msu.edu>
2019-08-31 00:07:52 +0300

    day10

---
+++ day10.py
@@ -0,0 +1,38 @@
+# Python Operators
+# Arithmetic Operators
+print(1+2, 3-5, 8*3, 4/2, 7//2, 8%3, 2**4)
+
+# Assignment Operators
+x=5
+x+=5
+print('+=', x)
+x-=5
+print('-=', x)
+x*=5
+print('*=', x)
+x/=5
+print('/=', x)
+x%=5
+print('%=', x)
+x = 5
+x//=5
+print('//=', x)
+# Bitwise kind of
+x=5
+x&=5
+print('&=', x)
+x = 2
+x |= 5
+print(x)
+x = 5
+x ^=3
+print('^=', x)
+x = 5 
+x >>=3
+print('>>=', x)
+x = 3
+x <<=3
+print('<<=', x)
+# Comparison
+print(1==2, 1>2, 1!=3, 3>=3)
+
