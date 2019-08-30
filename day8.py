0bb4c38adfb4b6ae70229e8a72eef45e437b3eff
Abdulsalam Alali <alaliab2@cse.msu.edu>
2019-08-30 23:46:50 +0300

    day8

---
+++ day8.py
@@ -0,0 +1,10 @@
+# Python String 2
+# remove white spaces
+a = "  Stuff Stuff Stuff    "
+print(a.strip())
+print(len(a)) #length of string
+print(len(a.strip()))
+print(a.lower()) # lower all
+print(a.upper()) 
+print(a.replace('St', 'B')) #replace St with B
+print(a.split()) # split into a list
