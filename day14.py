ac35c46aef8efe4f01aa652f4f346f3ece079094
Abdulsalam Alali <alaliab2@cse.msu.edu>
2019-09-02 16:40:06 +0300

    day14

---
+++ day14.py
@@ -0,0 +1,17 @@
+# Python Lists 2
+# Different ways of slicing
+lst2 = [1,45,23,98,23,545]
+print(lst2[:]) #the whole list
+print(lst2[-1]) # the last item
+print(lst2[2:]) # third item till the end
+
+# check if item exists
+if 45 in lst2:
+    print('45 is in our list')
+
+# repeating items in a list
+lst3 = [1,2,3]
+print(lst3*3) # repeat three times
+
+# operators on lists
+print(lst2+lst3)
