1e8994925ba5ab1446da0621a84fc3e372045e0e
Abdulsalam Alali <salam1416@gmail.com>
2019-09-14 16:19:17 +0300

    day25 and day26

---
+++ day25.py
@@ -0,0 +1,12 @@
+# Day 25
+set1 = {1,3,5,7,8}
+print(set1)
+# adding elements to a set
+set1.update({4,8,12})
+print(set1)
+# Deleting 8 from the set
+set1.remove(8)
+print(set1)
+# Clearing everything from the set
+set1.clear()
+print(set1)
