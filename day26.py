0210ec440dbbf80f4cab0fefd7a934965068c45c
Abdulsalam Alali <salam1416@gmail.com>
2019-09-14 16:19:37 +0300

    day25 and day26

---
+++ day26.py
@@ -0,0 +1,8 @@
+# Day 26
+dict1 = {'name':'pigeon', 'type':'bird', 'skin cover':'feathers'}
+print(dict1)
+# printing type
+print(dict1['type'])
+# changing skin cover
+dict1['skin cover'] = 'hair'
+print(dict1)
