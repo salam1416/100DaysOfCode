58865d7078bdb5233aa3e410f205821beb1d906b
Abdulsalam Alali <salam1416@gmail.com>
2019-09-19 19:11:34 +0300

    day32 &  day33

---
+++ day32.py
@@ -0,0 +1,6 @@
+# Printing numbers
+
+# odd numbers from 3 to 17
+print( 'A : B')
+for i in range(2,18,2):
+    print('{:<2}  {:<2}'.format(i+1, i))
---
+++ day33.py
@@ -0,0 +1,5 @@
+# Repeating day32 with another way
+
+print({i+1:i for i in range(2,18,2)})
+ 
+# as a dictionary
