303f06458ddf5ad4c110f05b77de709c8b758728
Abdulsalam Alali <salam1416@gmail.com>
2019-09-27 10:50:18 +0300

    day39

---
+++ day39.py
@@ -0,0 +1,10 @@
+# Creating power function n^power with recursion
+def exponential(power, num):
+    if power < 1:
+        return 1 # this will exit
+    else:
+        return num * exponential(power -1, num)
+
+print(exponential(3, 5))
+
+# Recursion is very confusing
