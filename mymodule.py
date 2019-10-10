d1e13bbc0db5222d5b847d05240edcba77d5d230
Abdulsalam Alali <salam1416@gmail.com>
2019-10-10 18:42:13 +0300

    day50

---
+++ mymodule.py
@@ -0,0 +1,12 @@
+# A module file to be used in day50
+def greeting(name):
+    print('Hello, ', name)
+
+# A module could also contain variables
+person1 = {
+    'name':'John',
+    'age':36,
+    'country':'Norway'
+}
+
+lst = [1,2,3,5,6,7,6,5,4,3,2,1]
