8a2ee26a0cd976bd2ebd85bfd592db22da0ae1df
Abdulsalam Alali <salam1416@gmail.com>
2019-11-08 11:09:22 +0300

    day74

---
+++ day74.py
@@ -0,0 +1,6 @@
+# reading and adding to file challenge
+
+# open file for appending
+fp = open('txt_day74.txt','a')
+fp.write(' The best way we learn everything is by practice and exercise questions')
+fp.close()
